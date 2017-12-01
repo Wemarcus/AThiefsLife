ca7b73b63b4f6e77b8aea05b73ef5a9f60a1344b
Francesco Cosma <cosmafrancesco93@gmail.com>
2017-12-01 18:42:45 +0100

    Added bullet shoot + enemy bugfix

---
+++ Assets/BulletDamage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletDamage : MonoBehaviour {
+
+	public int damage;
+	public BulletTag bulletTag;
+
+	void OnCollisionEnter(Collision collision){
+		Debug.Log ("proiettile collide con :" + collision.gameObject);
+		if (collision.gameObject.tag == "Enemy" && bulletTag == BulletTag.friendly) {
+			DealDamageToEnemy (collision.gameObject);
+			Destroy (this.gameObject);
+		}
+		if (collision.gameObject.tag == "Player" && bulletTag == BulletTag.foe) {
+			DealDamageToPlayer (collision.gameObject);
+			Destroy (this.gameObject);
+		}
+	}
+
+	void DealDamageToPlayer( GameObject playerTrg){
+		Player plr = playerTrg.GetComponent<Player> ();
+		MapHandler mh = FindObjectOfType<MapHandler> ();
+		plr.DealDamage (damage);
+		if (plr.currentHP <= 0) {
+			mh.players.Clear ();
+			Grid.GridMath.ChangeBlockType (Grid.GridMath.GetPlayerBlock (playerTrg), BlockType.Walkable);
+			Destroy (playerTrg);
+		}
+	}
+
+	void DealDamageToEnemy(GameObject enemy){
+		Enemy enm = enemy.GetComponent<Enemy> ();
+		enm.DealDamage (damage);
+		MapHandler mh = FindObjectOfType<MapHandler> ();
+		if (enm.currentHP <= 0) {
+			mh.targetList.Remove (enemy);
+			Grid.GridMath.ChangeBlockType (Grid.GridMath.GetEnemyBlock (enemy), BlockType.Walkable);
+			Destroy (enemy);
+		}
+	}
+}
