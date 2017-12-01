5899adbc178218746b0c7ad2bb4edf6b8f2444e6
Marco Rusciano <marco_rusciano@yahoo.it>
2017-12-01 20:03:53 +0100

    Added new characters, new animations, new animation controller and cover system

---
+++ Assets/Script Marco/Agent_Animation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent_Animation : MonoBehaviour {
+
+    public GameObject Sphere;
+    //private Animator agent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Cover":
+                Sphere.SetActive(true);
+                break;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        switch (other.tag)
+        {
+            case "Cover":
+                Sphere.SetActive(false);
+                break;
+        }
+    }
+}
