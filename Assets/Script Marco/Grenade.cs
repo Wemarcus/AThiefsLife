a34b0bc45088deb3f69f3f2518924fb246b34963
Marco Rusciano <marco_rusciano@yahoo.it>
2017-12-07 23:23:01 +0100

    Added four grenade prefab

--- Assets/Script Marco/Grenade.cs
+++ Assets/Script Marco/Grenade.cs
@@ -8,23 +8,14 @@ public class Grenade : MonoBehaviour {
     public AudioSource sound;
     public float radius = 5.0F;
     public float power = 10.0F;
-    public bool is_explosive;
+    public bool is_grenade;
+    public bool is_flash;
+    public bool is_C4;
+    public bool is_smoke;
 
     private bool block;
     private Vector3 explosionPos;
     private Collider[] colliders;
-
-    private void Update()
-    {
-        if (is_explosive)
-        {
-            Destroy(transform.parent.gameObject, 10.0f);
-        }
-        else
-        {
-            Destroy(transform.parent.gameObject, 15.0f);
-        }
-    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -33,9 +24,9 @@ public class Grenade : MonoBehaviour {
 
         Debug.Log("La granata collide con: " + other.gameObject);
 
-        if (other.tag == "Player" || other.tag == "Enemy")
+        if (!block && (other.tag == "Player" || other.tag == "Enemy"))
         {
-            gameObject.SetActive(false);
+            block = true;
             smoke_effect.SetActive(true);
 
             if (sound != null)
@@ -43,10 +34,8 @@ public class Grenade : MonoBehaviour {
                 sound.Play();
             }
 
-            if (!block && is_explosive)
+            if (is_grenade || is_C4)
             {
-                block = true;
-
                 foreach (Collider hit in colliders)
                 {
                     Debug.Log(hit.name);
@@ -59,10 +48,30 @@ public class Grenade : MonoBehaviour {
                         {
                             rb.useGravity = true;
                             rb.isKinematic = false;
+                            rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
+                        } else if(rb.tag != "Grenade")
+                        {
+                            rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
                         }
-                        rb.AddExplosionForce(power, explosionPos, radius, 3.0F);
                     }
                 }
+            }
+
+            if (is_flash)
+            {
+                Destroy(transform.parent.gameObject, 6.0f);
+            }
+            else if (is_smoke)
+            {
+                Destroy(transform.parent.gameObject, 13.0f);
+            }
+            else if (is_grenade)
+            {
+                Destroy(transform.parent.gameObject, 7.0f);
+            }
+            else if (is_C4)
+            {
+                Destroy(transform.parent.gameObject, 4.0f);
             }
         }
 
