72f0d24c8dbd8fa3d1cda433356982d3f952a7ef
Marco Rusciano <marco_rusciano@yahoo.it>
2017-12-07 16:59:37 +0100

    Added bullet physics, grenade explosion physics, sound effect, improved Master Scene and cover system

--- Assets/BulletDamage.cs
+++ Assets/BulletDamage.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletDamage : MonoBehaviour {
-
+    // Parte Francesco
 	public int damage;
 	public BulletTag bulletTag;
 
-    // MODIFICA MARCO => necessario passaggio all'OnTriggerEnter per animazioni..
+    // Parte Marco
+    public float radius = 5.0F;
+    public float power = 10.0F;
+    private bool block;
+    private Vector3 explosionPos;
+    private Collider[] colliders;
 
-	/*void OnCollisionEnter(Collision collision){
+    // MODIFICA MARCO => necessario passaggio all'OnTriggerEnter per animazioni.. e fusione con AddForce.cs
+
+    /*void OnCollisionEnter(Collision collision){
 		Debug.Log ("proiettile collide con :" + collision.gameObject);
 		if (collision.gameObject.tag == "Enemy" && bulletTag == BulletTag.friendly) {
 			DealDamageToEnemy (collision.gameObject);
@@ -21,7 +28,7 @@ public class BulletDamage : MonoBehaviour {
 		}
 	}*/
 
-    void OnTriggerEnter(Collider other)
+    /*void OnTriggerEnter(Collider other)
     {
         Debug.Log("proiettile collide con :" + other.gameObject);
         if (other.gameObject.tag == "Enemy" && bulletTag == BulletTag.friendly)
@@ -33,6 +40,91 @@ public class BulletDamage : MonoBehaviour {
         {
             DealDamageToPlayer(other.gameObject);
             Destroy(this.gameObject);
+        }
+    }*/
+
+    private void Update()
+    {
+        Destroy(this.gameObject, 3.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        explosionPos = transform.position;
+        colliders = Physics.OverlapSphere(explosionPos, radius);
+
+        Debug.Log("Il proiettile collide con :" + other.gameObject);
+
+        switch (other.tag)
+        {
+            case "Glass":
+                if (!block)
+                {
+                    block = true;
+
+                    foreach (Collider hit in colliders)
+                    {
+                        Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+                        if (rb != null)
+                        {
+                            if (rb.tag == "Glass")
+                            {
+                                rb.useGravity = true;
+                                rb.isKinematic = false;
+                            }
+                            //rb.AddExplosionForce(power, explosionPos, radius, 20.0F);
+                            rb.AddForce(transform.right * 100, ForceMode.Acceleration);
+                            Debug.Log("Explosive force applied to Glass");
+                        }
+                    }
+                    Destroy(this.gameObject);
+                }
+                break;
+
+            case "Props":
+                if (!block)
+                {
+                    block = true;
+
+                    foreach (Collider hit in colliders)
+                    {
+                        Rigidbody rb = hit.GetComponent<Rigidbody>();
+
+                        if (rb != null)
+                        {
+                            //rb.AddExplosionForce(power, explosionPos, radius, 20.0F);
+                            rb.AddForce(transform.right * 500, ForceMode.Acceleration);
+                            Debug.Log("Explosive force applied to Props");
+                        }
+                    }
+                    Destroy(this.gameObject);
+                }
+                break;
+
+            case "Wall":
+                Destroy(this.gameObject);
+                break;
+
+            case "Cover":
+                Destroy(this.gameObject);
+                break;
+
+            case "Enemy":
+                if (bulletTag == BulletTag.friendly)
+                {
+                    DealDamageToEnemy(other.gameObject);
+                    Destroy(this.gameObject);
+                }
+                break;
+
+            case "Player":
+                if (bulletTag == BulletTag.foe)
+                {
+                    DealDamageToPlayer(other.gameObject);
+                    Destroy(this.gameObject);
+                }
+                break;
         }
     }
 
---
+++ Assets/Script Marco/Sound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour {
+
+    public string tag_name;
+    private AudioSource sound;
+
+    private void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == tag_name)
+        {
+            sound.Play();
+        }
+    }
+}
