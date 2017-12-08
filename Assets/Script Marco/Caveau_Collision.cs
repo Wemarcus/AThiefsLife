fe78ba0808870179af197f867770bec8b5ca2366
Marco Rusciano <marco_rusciano@yahoo.it>
2017-12-08 12:25:52 +0100

    Improved caveau event in the Master Scene

---
+++ Assets/Script Marco/Caveau_Collision.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Caveau_Collision : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Caveau")
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<Rigidbody>().isKinematic = false;
+            GetComponent<NavMeshObstacle>().enabled = false;
+        }
+    }
+}
