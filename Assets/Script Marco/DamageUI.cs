6edfac07813c51968221b659082f2b4b2fa19c3e
Marco Rusciano <marco_rusciano@yahoo.it>
2017-12-11 16:04:50 +0100

    Improved Enemy and Employee Health Bar and Added Damage Popup

---
+++ Assets/Script Marco/DamageUI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DamageUI : MonoBehaviour {
+
+    private Animator anim;
+    private Text damage;
+
+	// Use this for initialization
+	void Start () {
+        Animator anim = GetComponent<Animator>();
+        AnimatorClipInfo[] clipInfo = anim.GetCurrentAnimatorClipInfo(0);
+        Destroy(gameObject, clipInfo[0].clip.length);
+        damage = anim.GetComponent<Text>();
+	}
+	
+	public void SetText(string text)
+    {
+        damage.text = text;
+    }
+}
