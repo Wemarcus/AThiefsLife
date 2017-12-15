d704f19f758b322938d16d495c52b77802628447
Francesco Cosma <cosmafrancesco93@gmail.com>
2017-12-15 11:54:40 +0100

    Added dynamic circle under players and enemies

---
+++ Assets/PlayerCircle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCircle : MonoBehaviour {
+
+	MapHandler mh;
+	GameObject currentPlayerMarked;
+	SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+		mh = FindObjectOfType<MapHandler> ();
+		currentPlayerMarked = this.gameObject.GetComponentInParent<Player> ().gameObject;
+		sr = this.gameObject.GetComponent<SpriteRenderer> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (mh.selectedPlayer == null || mh.selectedPlayer != currentPlayerMarked) {
+			sr.enabled = false;
+		}
+		if (mh.selectedPlayer != null && mh.selectedPlayer == currentPlayerMarked) {
+			sr.enabled = true;
+		}
+	}
+}
