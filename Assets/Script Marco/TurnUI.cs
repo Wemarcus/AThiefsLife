257c43fe4ea3cb589243561be60cb9bae49978be
Marco Rusciano <marco_rusciano@yahoo.it>
2017-12-24 17:37:33 +0100

    Added graphic feedback for turn

---
+++ Assets/Script Marco/TurnUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnUI : MonoBehaviour {
+
+    private Animator anim;
+
+    void Start () {
+        Animator anim = GetComponent<Animator>();
+        AnimatorClipInfo[] clipInfo = anim.GetCurrentAnimatorClipInfo(0);
+        Destroy(gameObject, clipInfo[0].clip.length);
+    }
+}
