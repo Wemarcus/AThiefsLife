4bdf8bcdd698715c853f4a7cff1697a40f9c43db
Francesco Cosma <cosmafrancesco93@gmail.com>
2017-12-28 16:01:00 +0100

    Bugfix turn + development of end stats

---
+++ assets/EndStatsHandle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndStatsHandle : MonoBehaviour {
+
+	public Text mapName;
+	public GameObject thiefContent;
+	public Text policemanKilled;
+	public Text employedKilled;
+	public Text moneyRobbed;
+	MapHandler mh;
+	bool setted;
+
+	// Use this for initialization
+	void Start () {
+		//setto nome mappa
+		//setto icone ladri
+		mh = FindObjectOfType<MapHandler> ();
+		policemanKilled.text = mh.policemanKilled.ToString();
+		employedKilled.text = mh.EmployedKilled.ToString();
+		moneyRobbed.text = mh.money.ToString();
+		this.gameObject.SetActive (false);
+		setted = true;
+	}
+
+	void OnEnable(){
+		if (setted) {
+			//setto nome mappa
+			//setto icone ladri
+			policemanKilled.text = mh.policemanKilled.ToString();
+			employedKilled.text = mh.EmployedKilled.ToString();
+			moneyRobbed.text = mh.money.ToString();
+		}
+	}
+}
