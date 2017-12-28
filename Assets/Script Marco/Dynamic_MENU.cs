777735ef704640ad6f78fd086216e6d28ade0093
Marco Rusciano <marco_rusciano@yahoo.it>
2017-12-28 23:17:47 +0100

    Improved Game Menu and Game Physics

---
+++ Assets/Script Marco/Dynamic_MENU.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dynamic_MENU : MonoBehaviour {
+
+    public GameObject prefab;
+    public Transform position;
+    public Transform target;
+    public float time;
+
+    private bool block;
+    private bool block_2;
+    private GameObject tmp;
+	
+	void Update () {
+        if (!block)
+        {
+            block = true;
+            tmp = Instantiate(prefab, position);
+            StartCoroutine(Time());
+        }
+
+        if (!block_2 && tmp.GetComponent<UnityStandardAssets.Characters.ThirdPerson.Dynamic_Menu_Swat>().floor)
+        {
+            block_2 = true;
+            tmp.GetComponent<UnityStandardAssets.Characters.ThirdPerson.Dynamic_Menu_Swat>().agent.enabled = true;
+            tmp.GetComponent<UnityStandardAssets.Characters.ThirdPerson.Dynamic_Menu_Swat>().SetTarget(target);
+        }
+    }
+
+    private IEnumerator Time()
+    {
+        yield return new WaitForSeconds(time);
+        block = false;
+        block_2 = false;
+    }
+}
