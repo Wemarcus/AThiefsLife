f1c1d20b205a2233b8072808bd03cd5c2bb7d233
Francesco Cosma <cosmafrancesco93@gmail.com>
2017-12-29 19:46:38 +0100

    Added bomb Ai type

--- Assets/Script Francesco/Enemy.cs
+++ Assets/Script Francesco/Enemy.cs
@@ -24,6 +24,12 @@ public class Enemy : MonoBehaviour {
 
 	public GameObject nearestPlayer;
 
+	[Header("Aoe Setup(only if weapon type is aoe)")]
+
+	public GameObject bombPrefab;
+	public int cdDuration;
+	public int bombRange;
+
 	public void Start(){
 		currentHP = maxHP;
 		if(!FindObjectOfType<MapHandler>().enemiesOnMap.Contains(this.gameObject))
@@ -61,6 +67,9 @@ public class Enemy : MonoBehaviour {
 			break;
 		case AiType.onlymovement:
 			StartCoroutine( OnlyMovementAICor ());
+			break;
+		case AiType.bomb:
+			StartCoroutine (BombAICor ());
 			break;
 		}
 	}
@@ -150,6 +159,140 @@ public class Enemy : MonoBehaviour {
 		LookNearestPlayer ();
 		FindObjectOfType<AIHandler>().RunNextAI();
 
+	}
+
+	private IEnumerator BombAICor(){
+		//playersTrg = GridMath.FindPlayers ();
+		playersTrg.Clear();
+		playersTrg.AddRange(FindObjectOfType<MapHandler>().playersOnMap);
+		playersTrg = GridFunc.HittablePlayers (this.gameObject, playersTrg,range);
+		MoveSpots = GridMath.FindWalkPathInRange (block, moveRange);
+		//Debug.Log (MoveSpots.Count);
+		GameObject target;
+		GameObject MoveSpot;
+
+		while (FindObjectOfType<MapHandler> ().PerformingAction) {
+			yield return new WaitForSeconds(1f);
+		}
+
+		if (playersTrg.Count > 0 && !this.GetComponent<Confusion>()) {
+			//hit
+			target = playersTrg[Random.Range(0,playersTrg.Count)];
+			//FindObjectOfType<MapHandler> ().ProvideDamageToPlayer (target, damage);
+			Grid.GridMath.RotateCharacter(this.gameObject,target);
+			yield return  new WaitForSeconds(0.5f);
+			FindObjectOfType<MapHandler> ().ThrowBombToPlayer (target, bombPrefab, damage, bombRange, cdDuration);
+			//FindObjectOfType<MapHandler>().FireBulletToPlayer(target,this.gameObject,damage); //TODO
+			yield return  new WaitForSeconds(1f);
+			//Debug.Log ("sto colpendo:" + target.name); // calcolo del danno TODO
+			//then move
+			//Debug.Log ("ho colpito:" + target.name + "ora mi muovo");
+
+			while (FindObjectOfType<MapHandler> ().PerformingAction) {
+				yield return new WaitForSeconds(1f);
+			}
+
+			List<GameObject> sortedMoveSpots = new List<GameObject> ();
+			Node n;
+			foreach (GameObject elem in MoveSpots) {
+				n = elem.GetComponent<Node> ();
+				if (n.isCover)
+					sortedMoveSpots.Add (elem);
+			}
+			if (sortedMoveSpots.Count > 0) {
+				if (playerIsNear()) {
+					sortedMoveSpots = SortSpotsByNearestPlayer (sortedMoveSpots);
+					MoveSpot = sortedMoveSpots [0];
+				}else{
+					MoveSpot = sortedMoveSpots[Random.Range(0,sortedMoveSpots.Count-1)];
+				}
+				if(MoveSpot)
+					GridMath.MoveEnemyToBlock (this.gameObject, MoveSpot);
+			} else if(MoveSpots.Count >0){
+				if (playerIsNear()) {
+					MoveSpots = SortSpotsByNearestPlayer (MoveSpots);
+					MoveSpot = MoveSpots [0];
+				}else{
+					MoveSpot = MoveSpots[Random.Range(0,MoveSpots.Count-1)];
+				}
+				if(MoveSpot)
+					GridMath.MoveEnemyToBlock (this.gameObject, MoveSpot);
+
+			}
+			/*MoveSpot = MoveSpots[Random.Range(0,MoveSpots.Count)];
+			if (MoveSpot) {
+				GridMath.MoveEnemyToBlock (this.gameObject, MoveSpot);
+				yield return  new WaitForSeconds(1f);
+			}*/
+
+			while (FindObjectOfType<MapHandler> ().PerformingAction) {
+				yield return  new WaitForSeconds(1f);
+			}
+
+		} else {
+			// move
+			//Debug.Log ("non potevo colpire nessuno, mi muovo");
+
+			List<GameObject> sortedMoveSpots = new List<GameObject> ();
+			Node n;
+			foreach (GameObject elem in MoveSpots) {
+				n = elem.GetComponent<Node> ();
+				if (n.isCover)
+					sortedMoveSpots.Add (elem);
+			}
+			if (sortedMoveSpots.Count > 0) {
+				if (playerIsNear()) {
+					sortedMoveSpots = SortSpotsByNearestPlayer (sortedMoveSpots);
+					MoveSpot = sortedMoveSpots [0];
+				}else{
+					MoveSpot = sortedMoveSpots[Random.Range(0,sortedMoveSpots.Count-1)];
+				}
+				if(MoveSpot)
+					GridMath.MoveEnemyToBlock (this.gameObject, MoveSpot);
+			} else if(MoveSpots.Count >0){
+				if (playerIsNear()) {
+					MoveSpots = SortSpotsByNearestPlayer (MoveSpots);
+					MoveSpot = MoveSpots [0];
+				}else{
+					MoveSpot = MoveSpots[Random.Range(0,MoveSpots.Count-1)];
+				}
+				if(MoveSpot)
+					GridMath.MoveEnemyToBlock (this.gameObject, MoveSpot);
+
+			}
+
+			/*MoveSpot = MoveSpots[Random.Range(0,MoveSpots.Count)];
+			if (MoveSpot) {
+				GridMath.MoveEnemyToBlock (this.gameObject, MoveSpot);
+				yield return  new WaitForSeconds(1f);
+			}*/
+			//try to hit
+
+			while (FindObjectOfType<MapHandler> ().PerformingAction) {
+				yield return  new WaitForSeconds(1f);
+			}
+
+			playersTrg = GridFunc.HittablePlayers(this.gameObject,playersTrg,range);
+			if (playersTrg.Count > 0 && !this.GetComponent<Confusion>()) {
+				target = playersTrg[Random.Range(0,playersTrg.Count)];
+				//Debug.Log ("posso colpire:" + target.name);
+				//Hit
+				target = playersTrg[Random.Range(0,playersTrg.Count)];
+				//Debug.Log ("sto colpendo:" + target.name); // calcolo del danno TODO
+				//FindObjectOfType<MapHandler> ().ProvideDamageToPlayer (target, damage);
+				Grid.GridMath.RotateCharacter(this.gameObject,target);
+				yield return  new WaitForSeconds(0.5f);
+				FindObjectOfType<MapHandler> ().ThrowBombToPlayer (target, bombPrefab, damage, bombRange, cdDuration);
+				//FindObjectOfType<MapHandler>().FireBulletToPlayer(target,this.gameObject,damage); //TODO
+			}
+		}
+		//chiama prossimo
+		/*while (FindObjectOfType<MapHandler> ().PerformingAction) {
+			yield return null;
+		}*/
+		yield return new WaitForSeconds(2f);
+		LookNearestPlayer ();
+		FindObjectOfType<AIHandler>().RunNextAI();
 	}
 
 	private IEnumerator BasicAICor(){
@@ -283,6 +426,8 @@ public class Enemy : MonoBehaviour {
 		LookNearestPlayer ();
 		FindObjectOfType<AIHandler>().RunNextAI();
 	}
+
+
 
 	/*public void OnAnimationPerform(bool b){
 		if (!b) {
