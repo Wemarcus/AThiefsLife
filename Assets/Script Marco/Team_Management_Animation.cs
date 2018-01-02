b030369303080eb44d090b50be18533bf0bd3b4a
Marco Rusciano <marco_rusciano@yahoo.it>
2018-01-02 19:59:00 +0100

    Improved Team Management Menu and Improved enemies waves

---
+++ Assets/Script Marco/Team_Management_Animation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Team_Management_Animation : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	public void Clicked()
+    {
+        anim.SetTrigger("SelectedPlayer");
+    }
+}
