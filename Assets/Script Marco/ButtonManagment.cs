4f899504f99d9d7183c424e8bf5c81871f0cfa1e
Marco Rusciano <marco_rusciano@yahoo.it>
2018-01-04 10:00:47 +0100

    Bugfix Load Menu

---
+++ Assets/Script Marco/ButtonManagment.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonManagment : MonoBehaviour {
+
+    public GameObject Fake_Background;
+    public Button Delete;
+    public Button Play;
+
+    public void ChangeButtonStatus()
+    {
+        Fake_Background.SetActive(false);
+        Delete.interactable = true;
+        Play.interactable = true;
+    }
+}
