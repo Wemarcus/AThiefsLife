212e575cdc7a92cbb903af3b4505f8a8f626f200
Marco Rusciano <marco_rusciano@yahoo.it>
2018-01-04 16:25:37 +0100

    Added Button in the Prison Menu

---
+++ Assets/Script Marco/SlerpRotation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlerpRotation : MonoBehaviour {
+
+    public GameObject Door;
+    public float rotation; //rotazione della porta
+    public float speed;
+    public bool open;
+
+    private Vector3 defaultRot;
+    private Vector3 openRot;
+
+    public void Start()
+    {
+        defaultRot = Door.transform.eulerAngles;
+        openRot = new Vector3(defaultRot.x, defaultRot.y + rotation, defaultRot.z);
+    }
+
+    public void Update()
+    {
+        if (open)
+        {
+            Door.transform.eulerAngles = Vector3.Slerp(Door.transform.eulerAngles, openRot, Time.deltaTime * speed);
+        }
+    }
+}
