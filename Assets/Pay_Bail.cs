f257b4183864d986771fba64c6b4b2bfe26faa4e
Marco Rusciano <marco_rusciano@yahoo.it>
2018-01-05 16:00:13 +0100

    Bugfix Prison Menu

--- Assets/Pay_Bail.cs
+++ Assets/Pay_Bail.cs
@@ -1,14 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pay_Bail : MonoBehaviour {
+
+    public GameObject moneyMenu; // popup non abbastanza soldi
+    public GameObject bailMenu; // popup pagamento cauzione
+
+    public GameObject fakeBackground;
+    public GameObject escapeButton;
+    public GameObject sentenceButton;
+    public GameObject bailButton;
+    public GameObject prisonDoor;
 
 	public void PayBail(){
 		if (MoneyAreEnought()) { // if se i soldi sono sufficenti a pagare la cauzione
 			CurrentGame.cg.PayBail (CurrentGame.cg.end.caution);
-		} else { // else se non hai abbastanza soldi
-			
+
+            fakeBackground.SetActive(true);
+            bailMenu.SetActive(false);
+            escapeButton.GetComponent<Button>().interactable = false;
+            sentenceButton.GetComponent<Button>().interactable = false;
+            bailButton.GetComponent<Button>().interactable = false;
+            prisonDoor.GetComponent<SlerpRotation>().OpenDoor();
+
+        } else { // else se non hai abbastanza soldi
+            bailMenu.SetActive(false);
+            moneyMenu.SetActive(true);
 		}
 	}
 
