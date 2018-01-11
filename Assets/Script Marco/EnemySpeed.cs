e9526395fa04a3393ade8cc5d8dcd73796d4feac
Marco Rusciano <marco_rusciano@yahoo.it>
2018-01-11 13:31:24 +0100

    Added FastForward Button

---
+++ Assets/Script Marco/EnemySpeed.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpeed : MonoBehaviour {
+
+	public void FastForward()
+    {
+        Time.timeScale = 2.0F;
+    }
+
+    public void NormalSpeed()
+    {
+        Time.timeScale = 1.0F;
+    }
+}
