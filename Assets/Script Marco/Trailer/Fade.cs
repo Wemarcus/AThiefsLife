8428da55356d02ab54d02b5c22e05e86107d27f4
Marco Rusciano <marco_rusciano@yahoo.it>
2018-01-11 18:57:33 +0100

    Created Trailer Scene

---
+++ Assets/Script Marco/Trailer/Fade.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Fade : MonoBehaviour
+{
+    private Image img;
+
+    private void Start()
+    {
+        img = GetComponent<Image>();
+        StartCoroutine(FadeImage(true));
+    }
+
+    IEnumerator FadeImage(bool fadeAway)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 2; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+
+            img.color = new Color(1, 1, 1, 0);
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                img.color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+    }
+}
---
+++ Assets/Script Marco/Trailer/TrailerAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerAI : MonoBehaviour {
+
+    public GameObject boss;
+    public GameObject sniper;
+    public GameObject tank;
+    public GameObject doctor;
+    public Transform[] target;
+
+	void Start () {
+        boss.GetComponent<UnityStandardAssets.Characters.ThirdPerson.AICharacterControlTrailer>().SetTarget(target[0]);
+        sniper.GetComponent<UnityStandardAssets.Characters.ThirdPerson.AICharacterControlTrailer>().SetTarget(target[1]);
+        tank.GetComponent<UnityStandardAssets.Characters.ThirdPerson.AICharacterControlTrailer>().SetTarget(target[2]);
+        doctor.GetComponent<UnityStandardAssets.Characters.ThirdPerson.AICharacterControlTrailer>().SetTarget(target[3]);
+    }
+	
+	void Update () {
+		
+	}
+}
