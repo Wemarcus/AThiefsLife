d8319b92705cf0d03da3d91461f81881c0e89aea
Marco Rusciano <marco_rusciano@yahoo.it>
2018-01-23 18:47:00 +0100

    Added reinforcements cameras

---
+++ Assets/Script Marco/reinforcements_Camera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reinforcements_Camera : MonoBehaviour {
+
+    public void CameraActivate()
+    {
+        GetComponent<Cinemachine.CinemachineVirtualCamera>().enabled = true;
+        FakeCamera();
+    }
+
+    private IEnumerator FakeCamera()
+    {
+        yield return new WaitForSeconds(5.0f);
+        GetComponent<Cinemachine.CinemachineVirtualCamera>().enabled = false;
+    }
+
+}
