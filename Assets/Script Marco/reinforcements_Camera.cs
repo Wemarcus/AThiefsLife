7b200535ff9f71c2b39db29dead7260a39a5e70f
Marco Rusciano <marco_rusciano@yahoo.it>
2018-01-24 00:44:52 +0100

    Completed cinematic cameras

--- Assets/Script Marco/reinforcements_Camera.cs
+++ Assets/Script Marco/reinforcements_Camera.cs
@@ -1,24 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class reinforcements_Camera : MonoBehaviour {
 
-	public void CameraActivate()
+    private PlayableDirector playable_director;
+    private bool control;
+
+    public void Start()
     {
-        if(GetComponent<Cinematic>() != null)
+        if (GetComponent<PlayableDirector>() != null)
         {
-            GetComponent<Cinemachine.CinemachineVirtualCamera>().enabled = true;
-            GetComponent<Cinematic>().isRunning = true;
-			StartCoroutine(FakeCamera());
+            playable_director = GetComponent<PlayableDirector>();
+            control = true;
         }
     }
 
-	private IEnumerator FakeCamera()
+    public void CameraActivate()
+    {
+        if (GetComponent<Cinematic>() != null)
+        {
+            Debug.Log("Quante volte entro?");
+            GetComponent<Cinemachine.CinemachineVirtualCamera>().enabled = true;
+            GetComponent<Cinematic>().isRunning = true;
+            //GetComponent<Cinemachine.CinemachineTrackedDolly>().m_PathPosition = 0;
+            if (control)
+            {
+                playable_director.time = 0;
+                playable_director.enabled = true;
+            }
+            else
+            {
+                StartCoroutine(FakeCamera());
+            }
+        }
+    }
+
+    private IEnumerator FakeCamera()
     {
         yield return new WaitForSeconds(5.0f);
         GetComponent<Cinemachine.CinemachineVirtualCamera>().enabled = false;
         GetComponent<Cinematic>().isRunning = false;
     }
-
 }
