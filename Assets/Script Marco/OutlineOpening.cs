7adc81add1b825439050d9edc3abc9ce1c2028b8
Marco Rusciano <marco_rusciano@yahoo.it>
2018-01-24 20:53:20 +0100

    Bugfix outline effect in the master scene

---
+++ Assets/Script Marco/OutlineOpening.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineOpening : MonoBehaviour {
+
+    public GameObject mainCamera;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!GetComponent<Cinemachine.CinemachineVirtualCamera>().enabled)
+        {
+            mainCamera.GetComponent<cakeslice.OutlineEffect>().enabled = true;
+            Destroy(this);
+        }
+
+	}
+}
